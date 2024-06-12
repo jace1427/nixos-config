afe5bded08a8c7082f3b07c77b52de070097b9ab
Justin Spidell <justintspidell@gmail.com>
2024-06-12 16:38:27 -0700

    46 current  2024-06-12 15:59:39  24.11.20240612.57d6973   6.6.32                          *

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -29,8 +29,7 @@
     };
     interactiveShellInit = ''
       set -g fish_key_bindings fish_default_key_bindings
-      bind \cc kill-whole-line repaint
-      bind \cd forward-char
+      bind \cH backward-kill-word
     '';
   };
 }
