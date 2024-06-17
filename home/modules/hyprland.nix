5ec900a146880aeee89ce3fdf58f3970f1e54394
Justin Spidell <justintspidell@gmail.com>
2024-06-17 11:25:51 -0700

    56 current  2024-06-17 11:06:59  24.11.20240612.57d6973   6.6.32                          *

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -8,5 +8,13 @@
     package = pkgs.hyprland;
     xwayland.enable = true;
     systemd.enable = true;
+
+    settings = {
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, F, exec, firefox"
+        "$mod, F, exec, Konsole"
+      ];
+    };
   };
 }
