a72f7c8f19c1aabb2376013356a32256c8813096
Justin Spidell <justintspidell@gmail.com>
2024-06-20 11:11:22 -0700

    66 current  2024-06-20 10:11:52  24.11.20240618.c00d587   6.6.34                          *

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -9,9 +9,9 @@
 
     settings = {
       monitor = [
-        "DP-1, 1920x1080@60, 0x0, 1"
+        "DP-3, 1920x1080@60, 0x0, 1"
+        "DP-1, 1920x1080@60, auto-left, 1"
         "DP-2, 1920x1080@60 auto-right, 1"
-        "DP-3, 1920x1080@60, auto-right, 1"
       ];
 
       "$terminal" = "kitty";
