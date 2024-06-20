9e1a63187804b2ff149953962fa04f367bc33f99
Justin Spidell <justintspidell@gmail.com>
2024-06-20 16:27:06 -0700

    71 current  2024-06-20 15:44:49  24.11.20240618.c00d587   6.6.34                          *

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -29,17 +29,14 @@
       ];
 
       general = {
-        gaps_in = 5;
-        gaps_out = 20;
+        gaps_in = 4;
+        gaps_out = 4;
 
-        border_size = 2;
+        border_size = 0;
+        resize_on_border = false;
 
-        # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
-
-        # Set to true enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false;
 
         # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
         allow_tearing = false;
@@ -47,18 +44,17 @@
         layout = "dwindle";
       };
 
-      # https://wiki.hyprland.org/Configuring/Variables/#decoration
       decoration = {
         rounding = 10;
-
-        # Change transparency of focused and unfocused windows
         active_opacity = 1.0;
-        inactive_opacity = 1.0;
+        inactive_opacity = 0.9;
 
         drop_shadow = true;
         shadow_range = 4;
         shadow_render_power = 3;
         "col.shadow" = "rgba(1a1a1aee)";
+
+        dim_inactive = true;
 
         # https://wiki.hyprland.org/Configuring/Variables/#blur
         blur = {
@@ -107,10 +103,9 @@
       # https://wiki.hyprland.org/Configuring/Variables/#input
       input = {
         kb_layout = "us";
-        follow_mouse = 1;
+        follow_mouse = 2;
         sensitivity = -1; # (-1.0, 1.0), 0 means no modification.
         accel_profile = "flat";
-        force_no_accel = 1;
       };
 
       # https://wiki.hyprland.org/Configuring/Variables/#gestures
@@ -119,6 +114,8 @@
       };
 
       windowrulev2 = "suppressevent maximize, class:.*"; # You'll probably like this.
+
+      disable_splash_rendering = true;
     };
   };
 }
