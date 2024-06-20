f613d19c1148a54247b72eadd836491995282d6b
Justin Spidell <justintspidell@gmail.com>
2024-06-20 13:27:10 -0700

    68 current  2024-06-20 12:43:01  24.11.20240618.c00d587   6.6.34                          *

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -12,13 +12,12 @@
     settings = {
       monitor = [
         "DP-1,preferred,auto,auto"
-        "DP-2,preferred,auto-left,auto"
-        "DP-3,preferred,auto-right,auto"
+        "DP-2,preferred,auto-right,auto"
+        "DP-3,preferred,auto-left,auto"
       ];
 
       "$terminal" = "kitty";
       "$filemanager" = "dolphin";
-      "$menu" = "rofi";
 
       exec-once = [
         "$terminal"
@@ -142,14 +141,10 @@
 
       # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
       bind = [
-        "$mainMod, Q, exec, $terminal"
+        "$mainMod, K, exec, $terminal"
         "$mainMod, C, killactive,"
-        "$mainMod, M, exit,"
-        "$mainMod, E, exec, $fileManager"
-        "$mainMod, V, togglefloating,"
-        "$mainMod, R, exec, $menu"
-        "$mainMod, P, pseudo," # dwindle
-        "$mainMod, J, togglesplit," # dwindle
+        "$mainMod, ESC, exit,"
+        "$mainMod, F, exec, $fileManager"
         "$mainMod, S, exec, rofi -show drun -show-icons"
 
         # Move focus with mainMod + arrow keys
@@ -162,29 +157,11 @@
         "$mainMod, 1, workspace, 1"
         "$mainMod, 2, workspace, 2"
         "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
         "$mainMod SHIFT, 1, movetoworkspace, 1"
         "$mainMod SHIFT, 2, movetoworkspace, 2"
         "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-        # Example special workspace (scratchpad)
-        "$mainMod, S, togglespecialworkspace, magic"
-        "$mainMod SHIFT, S, movetoworkspace, special:magic"
 
         # Scroll through existing workspaces with mainMod + scroll
         "$mainMod, mouse_down, workspace, e+1"
@@ -196,15 +173,6 @@
         "$mainMod, mouse:272, movewindow"
         "$mainMod, mouse:273, resizewindow"
       ];
-
-      # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-      # See https://wiki.hyprland.org/Configuring/Workspace-Rules/ for workspace rules
-
-      # Example windowrule v1
-      # windowrule = float, ^(kitty)$
-
-      # Example windowrule v2
-      # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
 
       windowrulev2 = "suppressevent maximize, class:.*"; # You'll probably like this.
     };
