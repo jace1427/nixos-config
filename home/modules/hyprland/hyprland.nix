d6064b2e11a71f23126d24570f1368196fdfe8fc
Justin Spidell <justintspidell@gmail.com>
2024-06-20 16:12:35 -0700

    71 current  2024-06-20 15:44:49  24.11.20240618.c00d587   6.6.34                          *

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -108,7 +108,7 @@
       input = {
         kb_layout = "us";
         follow_mouse = 1;
-        sensitivity = -0.5; # -1.0 - 1.0, 0 means no modification.
+        sensitivity = -1; # (-1.0, 1.0), 0 means no modification.
         accel_profile = "flat";
         force_no_accel = 1;
       };
