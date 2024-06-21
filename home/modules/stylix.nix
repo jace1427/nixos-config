92f382387a9b71bd53d8fb9dcfa8e3fba6cdd88c
Justin Spidell <justintspidell@gmail.com>
2024-06-20 22:27:16 -0700

    75 current  2024-06-20 22:20:10  24.11.20240618.c00d587   6.6.34                          *

--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -4,6 +4,10 @@
   config,
   ...
 }: {
-  stylix.image = ../../wallpapers/a_city_with_a_pyramid_over_it.png;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  stylix = {
+    enable = true;
+
+    image = ../../wallpapers/a_city_with_a_pyramid_over_it.png;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  };
 }
