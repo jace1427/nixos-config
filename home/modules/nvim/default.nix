4568fa4fee3a968f52a29b821ae73a1d588a04b7
Justin Spidell <justintspidell@gmail.com>
2024-06-26 09:42:20 -0700

    139 current  2024-06-26 09:42:14  24.11.20240618.c00d587   6.6.34                          *

--- home/modules/nvim/default.nix
+++ home/modules/nvim/default.nix
@@ -6,12 +6,6 @@
     vimAlias = true;
     vimdiffAlias = true;
     defaultEditor = true;
-
-    extraLuaConfig = ''
-      vim.g.mapleader = ' '
-      vim.g.maplocalleader = ' '
-      vim.g.have_nerd_font = true
-    '';
   };
 
   imports = [
--- home/modules/nvim/options.nix
+++ home/modules/nvim/options.nix
@@ -2,6 +2,13 @@
 {
   programs.neovim = {
     extraConfig = ''
+      set wildcharm=<tab>
+      set smartcase
+      set tabstop = 4
+      set expandtab
+      set softtabstop=0
+      set shiftwidth=0
+
       nmap <space>b :buffers<CR>
       nmap <C-l> :bnext<CR>
       nmap <C-h> :bprev<CR>
@@ -15,6 +22,9 @@
     '';
 
     extraLuaConfig = ''
+      vim.g.mapleader = ' '
+      vim.g.maplocalleader = ' '
+      vim.g.have_nerd_font = true
       vim.opt.number = true
       vim.opt.relativenumber = true
       vim.opt.showmode = false
@@ -22,21 +32,15 @@
       vim.opt.breakindent = true
       vim.opt.undofile = true
       vim.opt.ignorecase = true
-      vim.opt.smartcase = true
       vim.opt.signcolumn = 'yes'
       vim.opt.updatetime = 250
       vim.opt.timeoutlen = 300
       vim.opt.splitright = true
       vim.opt.splitbelow = true
       vim.opt.list = true
-      vim.opt.listchars = { tab = '>> ', trail = '*', nbsp = '_' }
       vim.opt.inccommand = 'split'
       vim.opt.cursorline = true
       vim.opt.scrolloff = 10
-      vim.opt.tabstop = 4
-      vim.opt.expandtab
-      vim.opt.softtabstop = 0
-      vim.opt.shiftwidth = 0
     '';
   };
 }
