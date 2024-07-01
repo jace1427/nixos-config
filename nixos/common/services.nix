a2459caa1f03e16b25e1da206f94b7b0735c69fe
Justin Spidell <justintspidell@gmail.com>
2024-06-30 20:26:04 -0700

    193 current  2024-06-29 21:54:31  24.11.20240624.2893f56   6.6.35                          *

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -41,7 +41,7 @@
       enable = true;
       pulse.enable = true;
       jack.enable = true;
-      wireplumber.enable = true;
+      # wireplumber.enable = true;
       alsa = {
         enable = true;
         support32Bit = true;
