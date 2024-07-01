1c5a4afbe2ae57e6ecd950483b929aa42a23dbf7
Justin Spidell <justintspidell@gmail.com>
2024-06-30 21:51:46 -0700

    195 current  2024-06-30 21:51:35  24.11.20240624.2893f56   6.6.35                          *

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -51,6 +51,21 @@
     minecraft-server = {
       enable = true;
       eula = true;
+      declarative = true;
+      whitelist = {
+        jace142 = "9c3c024d-1577-4280-9a04-b99e460b77d1";
+        BlazingHack_ = "5dac4040-89e7-4d8e-9e3a-bb4d01c1a82d";
+        petitesalope = "0c1cacea-37d4-4790-aa99-759c02f5a7f6";
+      };
+      openFirewall = true;
+      serverProperties = {
+        server-port = 43000;
+        difficutly = 2;
+        gamemode = 0;
+        max-players = 20;
+        motd = ":)";
+        white-list = true;
+      };
     };
   };
 }
