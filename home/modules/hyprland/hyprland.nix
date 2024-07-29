5c4cb6e3e1d137b867f6867596b9edd5005e52b8
Justin Spidell <justintspidell@gmail.com>
2024-07-29 16:55:43 -0700

    257 current  2024-07-29 16:55:32  24.11.20240727.b73c222   6.6.42                          *

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -49,8 +49,8 @@
 
         blur = {
           enabled = true;
-          size = 5;
-          passes = 5;
+          size = 9;
+          passes = 3;
           vibrancy = 0.1696;
           ignore_opacity = true;
           popups = true;
