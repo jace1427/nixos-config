b719662affac3fe42f1f947e0651fc024ce9d60e
Justin Spidell <justintspidell@gmail.com>
2024-07-29 16:57:38 -0700

    258 current  2024-07-29 16:57:27  24.11.20240727.b73c222   6.6.42                          *

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -131,7 +131,7 @@
       windowrulev2 = [
         "suppressevent maximize, class:.*" # You'll probably like this.
         "opacity 1.0 override 1.0 override 1.0 override,title:(.*)(YouTube)(.*)"
-        "opactiy 1.0 override 1.0 override 1.0 override,title:(.*)(World of Warcraft)(.*)"
+        "opacity 1.0 override 1.0 override 1.0 override,title:(.*)(World of Warcraft)(.*)"
       ];
     };
   };
